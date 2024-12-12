f0f48f4a9f05c302d8fc3106c8f593765b89f298
Shrija <lbajracharya2019@gmail.com>
2024-12-12 13:39:32 +0545

    added chart

--- src/components/Previous.js
+++ src/components/Previous.js
@@ -3,30 +3,45 @@ import imgpopulation from './bargraph.jpg'
 import './Previous.css';
 // import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router';
-import { Chart as ChartJS } from 'chart.js';
+import { Chart as ChartJS,ArcElement,Tooltip,Legend, plugins } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import { PieChartData } from './Data';
+ChartJS.register(ArcElement, Tooltip,Legend);
+
+export const PieChart=()=>{
+     
+    const options={
+      plugins : {
+        legend : {
+          position : "right",
+          labels : {
+            boxWidth : 30,
+            padding : 20
+          }
+        }
+      }
+    };
+   
+    
+    return <Pie options={options} data={PieChartData}/>;
+}
 function Previous() {
+
   return (
     <>
-    <div className="image">
-    <img src={imgpopulation} alt="img" />
-    {/* <Pie 
-    data={{
-      labels:["Bagmati","Gandaki","Lumbini","Karnali","Sudurpashchim","Madhesh","Koshi"],
-      datasets:[
-        {
-          "Bagmati":6084042,
-          "Gandaki":2479745,
-          "Lumbini":5124225,
-          "Karnali":1694889,
-          "Sudurpashchim":2711270,
-          "Madhesh":6126288,
-          "Koshi":4972021
-        }
-      ],
-      borderRadius:5,
-    }} */}
-    {/* /> */}
+    <br />
+   <center> <h3>According to Year 2021 AD (2078 BS)</h3></center>
+   <br />
+
+    <div className="image" >
+      <PieChart/>
+    {/* <canvas id="myChart" className="bg-dark"ref={canvasRef}></canvas> */}
+{/* const data = [
+  {"bag",45}
+]
+    */}
+    {/* <img src={imgpopulation} alt="img" /> */}
+   
     <div>
     <NavLink to="/age" className="links"><button type="button" className="btn btn-primary"> Based on age</button></NavLink>
     <NavLink to="/literacy" className="links"><button type="button" className="btn btn-info"> Based on literacy</button></NavLink>
